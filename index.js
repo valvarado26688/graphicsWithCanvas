347211d16d025cd1c09ddf8977cad89b546c8c40
Victor Alvarado <valvarado26688@gmail.com>
2025-02-14 00:23:34 -0500

    finished project

--- index.js
+++ index.js
@@ -4,30 +4,55 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let ctx = canvas.getContext("2d");
-// console.log(ctx);
 
-//first circle, good circle
-// ctx.beginPath();
-// ctx.arc(100, 100, 30, 0, Math.PI * 2, false);
-// ctx.strokeStyle = "green";
-// ctx.stroke();
+
+window.addEventListener("resize", (e) => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    initialization();
+})
+
+
+let mouse = {x: undefined, y: undefined};
+window.addEventListener("mousemove", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+    // console.log(mouse);
+});
+
+let colorArray = [ 
+    "#F2059F",
+    "#F205CB",
+    "#EE05F2", 
+    "#9305F2", 
+    "#0D0D0D"
+]
+
+
+let minRadius = 2;
+let maxRadius = 50;
 
 //class helps streamline the proccess of making however many circles i want
 class Circle {
-    constructor(x, y, dx, dy, radius) {
+    constructor(x, y, dx, dy, radius, maxRadius) {
         this.x = x;
         this.y = y;
         this.dx = dx; //dx dy represents velocity
         this.dy = dy;
         this.radius = radius;
+        this.maxRadius = maxRadius;
+        this.minRadius = radius;
+        this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
     }
-
+    
     //these methods will draw and increment each circle's position
     drawCircle() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        ctx.strokeStyle = "green";
-        ctx.stroke();
+        // ctx.strokeStyle = "green";
+        // ctx.stroke();
+        ctx.fillStyle = this.color;
+        ctx.fill();
     }
     updateCircle() {
         this.drawCircle();
@@ -39,6 +64,16 @@ class Circle {
         if(this.y + this.radius > innerHeight || this.y - this.radius < 0) {
             this.dy = -this.dy;
         }
+        
+        //interactivity
+        if(this.x - mouse.x < 50 && this.x - mouse.x > -50 && this.y - mouse.y < 50 && this.y - mouse.y > -50) {
+            if(this.radius < this.maxRadius) { //this should be specified as a "max radius"
+                this.radius += 1;
+            }
+        } else if(this.radius > this.minRadius) {
+            this.radius -= 1;
+        }
+        
     }
 }
 
@@ -47,38 +82,38 @@ class Circle {
 //circles info will be stored in an array 
 let generatedCircles = [];
 
-//loop iterates how many circles i want to spawn
-for(let i = 0; i < 100; i++) {
-    radius = 30;
+function initialization() {
+generatedCircles = [];
+
+//loop iterates how many circles I want to spawn
+for(let i = 0; i < 1000; i++) {
+    radius = Math.random() * 3 + 1;
 
     //random starting points
-    //math ensures the center of the circle is (radius) away from borders
     let x = Math.random() * (innerWidth - (radius * 2)) + radius; 
     let y = Math.random() * (innerHeight - (radius * 2)) + radius;
-    //random velocity
-    let dx = (Math.random() - 0.5) * 5;
-    let dy = (Math.random() - 0.5) * 5;
+    let dx = (Math.random() - 0.5) * 3;
+    let dy = (Math.random() - 0.5) * 3;
+
     //new circle with fresh coordinates and velocity
-    let circle = new Circle(x, y, dx, dy, radius);
+    let circle = new Circle(x, y, dx, dy, radius, maxRadius, minRadius);
 
     //circle added to the array 
     generatedCircles.push(circle);
 }
-
-console.log(generatedCircles);
+}
 
 
 //animation
 function animate() {
     requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-    
-    //for every circle that is stored in the array, execute updateCircle()
+    ctx.clearRect(0, 0, innerWidth, innerHeight,);
+
     generatedCircles.forEach((e) => {
         e.updateCircle();
     })
     
 }
 
-
+initialization();
 animate();
